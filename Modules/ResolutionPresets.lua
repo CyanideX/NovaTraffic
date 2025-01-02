13184933e617a9d478f05488fd9feb18f9a50390
CyanideX <3106700+CyanideX@users.noreply.github.com>
2025-01-02 09:23:04 -0800

    Update resolution presets for readability

--- Modules/ResolutionPresets.lua
+++ Modules/ResolutionPresets.lua
@@ -2,57 +2,98 @@ local ResolutionPresets = {}
 
 function ResolutionPresets.Set(width, height)
     local presets = {
-      --{ 1,    2,    3, 4, 5, 6, 7, 8, 9, 10, 11,   12,  13, 14, 15,   16, 17, 18,  19,  20, 21, 22, 23,  24,  25,  26,  27, 28, 29, 30, 31, 32,  33, 34, 35, 36,  37, 38, 39,   40, 41 },
-        { 3840, 2160, 8, 6, 5, 5, 6, 7, 1, 1,  0.7,  140, 34, 36, 0.62, 1,  6,  352, 396, 33, 34, 6,  650, 263, 350, 7.5, 9,  8,  5,  34, 34, 30, 140, 36, 36, 215, 10, 1,  3,    65, 350 },
-        { 2560, 1440, 8, 6, 1, 3, 6, 7, 1, 1,  0.45, 122, 28, 28, 0.85, 1,  8,  292, 324, 29, 34, 4,  500, 222, 298, 7.5, 8,  8,  3,  32, 32, 18, 130, 34, 36, 185, 10, 1,  3.5,  45, 300 },
-        { 1920, 1080, 5, 4, 1, 4, 6, 6, 1, 1,  0.5,  100, 24, 24, 0.85, 1,  0,  237, 258, 21, 30, 4,  400, 169, 232, 4.8, 9,  8,  6,  27, 27, 16, 100, 30, 22, 147, 6,  1,  1.75, 35, 232 },
-        { 0,    0,    5, 4, 1, 4, 6, 6, 1, 1,  0.5,  100, 24, 24, 0.85, 1,  0,  237, 258, 21, 30, 4,  400, 169, 232, 4.8, 9,  8,  6,  27, 27, 16, 100, 30, 22, 147, 6,  1,  3.5,  35, 232 },
+        buttonHeight = { 34, 28, 24, 24 },
+        buttonPaddingRight = { 33, 29, 21, 21 },
+        buttonRadius = { 8, 7, 5, 5 },
+        buttonWidth = { 140, 122, 100, 100 },
+        customFontScale = { 0.62, 0.85, 0.85, 0.85 },
+        defaultFontScale = { 1, 1, 1, 1 },
+        dummySpacingYValue = { 6, 8, 0, 0 },
+        exportDebugButtonYOffset = { 65, 45, 35, 35 },
+        exportDebugFrameYMulti = { 3, 3.5, 1.75, 3.5 },
+        exportDebugValue = { 1, 1, 1, 1 },
+        framePaddingXValue = { 5, 1, 1, 1 },
+        framePaddingYValue = { 5, 3, 4, 4 },
+        frameTabPaddingXValue = { 9, 8, 9, 9 },
+        frameTabPaddingYValue = { 8, 8, 8, 8 },
+        glyphAlignYValue = { 0.7, 0.45, 0.5, 0.5 },
+        glyphButtonHeight = { 34, 32, 27, 27 },
+        glyphButtonWidth = { 34, 32, 27, 27 },
+        glyphFramePaddingXValue = { 6, 6, 6, 6 },
+        glyphFramePaddingYValue = { 7, 7, 6, 6 },
+        glyphItemSpacingXValue = { 1, 1, 1, 1 },
+        glyphItemSpacingYValue = { 1, 1, 1, 1 },
+        invisibleButtonHeight = { 36, 36, 22, 22 },
+        invisibleButtonWidth = { 36, 34, 30, 30 },
+        itemSpacingXValue = { 8, 8, 5, 5 },
+        itemSpacingYValue = { 6, 6, 4, 4 },
+        itemTabSpacingYValue = { 5, 3, 6, 6 },
+        searchPaddingXValue = { 34, 34, 30, 30 },
+        searchPaddingYValue = { 6, 4, 4, 4 },
+        sliderHeight = { 10, 10, 6, 6 },
+        timeSliderXPadding = { 30, 18, 16, 16 },
+        toggleSpacingXValue = { 140, 130, 100, 100 },
+        uiCloudCustomizerHeight = { 350, 300, 232, 232 },
+        uiMinHeight = { 396, 324, 258, 258 },
+        uiMinWidth = { 352, 292, 237, 237 },
+        uiTimeHourRightPadding = { 7.5, 7.5, 4.8, 4.8 },
+        uiTimeMaxHeight = { 350, 298, 232, 232 },
+        uiTimeMinHeight = { 263, 222, 169, 169 },
+        uiTimeMinWidth = { 650, 500, 400, 400 },
+        weatherControlYOffset = { 215, 185, 147, 147 }
     }
 
-    for _, preset in ipairs(presets) do
-        if width >= preset[1] and height >= preset[2] then
-            itemSpacingXValue = preset[3]
-            itemSpacingYValue = preset[4]
-            framePaddingXValue = preset[5]
-            framePaddingYValue = preset[6]
-            glyphFramePaddingXValue = preset[7]
-            glyphFramePaddingYValue = preset[8]
-            glyphItemSpacingXValue = preset[9]
-            glyphItemSpacingYValue = preset[10]
-            glyphAlignYValue = preset[11]
-            buttonWidth = preset[12]
-            buttonHeight = preset[13]
-            unused = preset[14]
-            customFontScale = preset[15]
-            defaultFontScale = preset[16]
-            dummySpacingYValue = preset[17]
-            uiMinWidth = preset[18]
-            uiMinHeight = preset[19]
-            buttonPaddingRight = preset[20]
-            searchPaddingXValue = preset[21]
-            searchPaddingYValue = preset[22]
-            uiTimeMinWidth = preset[23]
-            uiTimeMinHeight = preset[24]
-            uiTimeMaxHeight = preset[25]
-            uiTimeHourRightPadding = preset[26]
-            frameTabPaddingXValue = preset[27]
-            frameTabPaddingYValue = preset[28]
-            itemTabSpacingYValue = preset[29]
-            glyphButtonWidth = preset[30]
-            glyphButtonHeight = preset[31]
-            timeSliderXPadding = preset[32]
-            toggleSpacingXValue = preset[33]
-            invisibleButtonWidth = preset[34]
-            invisibleButtonHeight = preset[35]
-            weatherControlYOffset = preset[36]
-            sliderHeight = preset[37]
-            exportDebugValue = preset[38]
-            exportDebugFrameYMulti = preset[39]
-            exportDebugButtonYOffset = preset[40]
-            uiCloudCustomizerHeight = preset[41]
-            break
+    local function getPresetValue(presetTable)
+        if width >= 3840 then
+            return presetTable[1]
+        elseif width >= 2560 then
+            return presetTable[2]
+        elseif width >= 1920 then
+            return presetTable[3]
+        else
+            return presetTable[4]
         end
     end
+
+    buttonHeight = getPresetValue(presets.buttonHeight)
+    buttonPaddingRight = getPresetValue(presets.buttonPaddingRight)
+    buttonRadius = getPresetValue(presets.buttonRadius)
+    buttonWidth = getPresetValue(presets.buttonWidth)
+    customFontScale = getPresetValue(presets.customFontScale)
+    defaultFontScale = getPresetValue(presets.defaultFontScale)
+    dummySpacingYValue = getPresetValue(presets.dummySpacingYValue)
+    exportDebugButtonYOffset = getPresetValue(presets.exportDebugButtonYOffset)
+    exportDebugFrameYMulti = getPresetValue(presets.exportDebugFrameYMulti)
+    exportDebugValue = getPresetValue(presets.exportDebugValue)
+    framePaddingXValue = getPresetValue(presets.framePaddingXValue)
+    framePaddingYValue = getPresetValue(presets.framePaddingYValue)
+    frameTabPaddingXValue = getPresetValue(presets.frameTabPaddingXValue)
+    frameTabPaddingYValue = getPresetValue(presets.frameTabPaddingYValue)
+    glyphAlignYValue = getPresetValue(presets.glyphAlignYValue)
+    glyphButtonHeight = getPresetValue(presets.glyphButtonHeight)
+    glyphButtonWidth = getPresetValue(presets.glyphButtonWidth)
+    glyphFramePaddingXValue = getPresetValue(presets.glyphFramePaddingXValue)
+    glyphFramePaddingYValue = getPresetValue(presets.glyphFramePaddingYValue)
+    glyphItemSpacingXValue = getPresetValue(presets.glyphItemSpacingXValue)
+    glyphItemSpacingYValue = getPresetValue(presets.glyphItemSpacingYValue)
+    invisibleButtonHeight = getPresetValue(presets.invisibleButtonHeight)
+    invisibleButtonWidth = getPresetValue(presets.invisibleButtonWidth)
+    itemSpacingXValue = getPresetValue(presets.itemSpacingXValue)
+    itemSpacingYValue = getPresetValue(presets.itemSpacingYValue)
+    itemTabSpacingYValue = getPresetValue(presets.itemTabSpacingYValue)
+    searchPaddingXValue = getPresetValue(presets.searchPaddingXValue)
+    searchPaddingYValue = getPresetValue(presets.searchPaddingYValue)
+    sliderHeight = getPresetValue(presets.sliderHeight)
+    timeSliderXPadding = getPresetValue(presets.timeSliderXPadding)
+    toggleSpacingXValue = getPresetValue(presets.toggleSpacingXValue)
+    uiCloudCustomizerHeight = getPresetValue(presets.uiCloudCustomizerHeight)
+    uiMinHeight = getPresetValue(presets.uiMinHeight)
+    uiMinWidth = getPresetValue(presets.uiMinWidth)
+    uiTimeHourRightPadding = getPresetValue(presets.uiTimeHourRightPadding)
+    uiTimeMaxHeight = getPresetValue(presets.uiTimeMaxHeight)
+    uiTimeMinHeight = getPresetValue(presets.uiTimeMinHeight)
+    uiTimeMinWidth = getPresetValue(presets.uiTimeMinWidth)
+    weatherControlYOffset = getPresetValue(presets.weatherControlYOffset)
 end
 
 return ResolutionPresets
